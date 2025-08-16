0a5a09dd208cb5fb05f4803d63483489861b2254
Samuel-Fernande1010 <samuelfernandes2406@gmail.com>
2025-08-16 19:17:39 -0300

    commit feito para adicionar a calculadora de custo de viagem

--- Calculadora-IMC.py
+++ Calculadora-IMC.py
@@ -1,7 +1,23 @@
-peso = float(input('Digite o seu peso em kg: '))
-altura = float(input('digite a sua altura em metros: '))
+def calculadora(x, y, n):
 
-imc = peso / (altura * altura)
+    if n == '1':
+        result = x * y
+    elif n == '2':
+        result = x + y
+    elif n == '3':
+        result = x - y
 
-print(f'Seu IMC é {imc:.2f}')
+    return result
 
+# --- Código para interagir com o usuário ---
+print("Escolha a operação:")
+print("1 - Multiplicação")
+print("2 - Adição")
+print("3 - Subtração")
+
+opcao = input("Qual opção deseja? (1, 2 ou 3): ")
+n1 = float(input("Escreva o primeiro n°: "))
+n2 = float(input("Escreva o segundo n°: "))
+
+r = calculadora(n1, n2, opcao)
+print("O resultado é:", r)
